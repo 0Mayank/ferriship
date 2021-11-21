516652b2a3679660f510382e6bbf21f1987eb480
Mayank Tomar <40265635+0Mayank@users.noreply.github.com>
2021-11-21 19:50:58 +0530

    lasers go brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,13 @@
 use bevy::prelude::*;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
+const LASER_SPRITE: &str = "laser_a_01.png";
+const TIME_STEP: f32 = 1. / 60.;
 
 // region: Resources
 pub struct Materials {
-    player_materials: Handle<ColorMaterial>,
+    player: Handle<ColorMaterial>,
+    laser: Handle<ColorMaterial>,
 }
 
 pub struct WinSize {
@@ -14,6 +17,19 @@ pub struct WinSize {
     w: f32,
 }
 // endregion: Resources
+
+// region: Components
+struct Player;
+struct PlayerReadyToFire(bool);
+struct Laser;
+struct Speed(f32);
+
+impl Default for Speed {
+    fn default() -> Self {
+        Self(500.)
+    }
+}
+// endregion: Components
 
 fn main() {
     App::build()
@@ -30,6 +46,9 @@ fn main() {
             "game_setup_actors",
             SystemStage::single(player_spawn.system()),
         )
+        .add_system(player_movement.system())
+        .add_system(player_fire.system())
+        .add_system(laser_movement.system())
         .run();
 }
 
@@ -46,7 +65,8 @@ fn setup(
 
     // create the main resources
     commands.insert_resource(Materials {
-        player_materials: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        laser: materials.add(asset_server.load(LASER_SPRITE).into()),
     });
 
     commands.insert_resource(WinSize {
@@ -58,13 +78,79 @@ fn setup(
 fn player_spawn(mut commands: Commands, materials: Res<Materials>, window: Res<WinSize>) {
     // spawn a sprite
     let bottom = -window.h / 2.;
-    commands.spawn_bundle(SpriteBundle {
-        material: materials.player_materials.clone(),
-        transform: Transform {
-            translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
-            scale: Vec3::new(0.5, 0.5, 1.),
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.player.clone(),
+            transform: Transform {
+                translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
+                scale: Vec3::new(0.5, 0.5, 1.),
+                ..Default::default()
+            },
             ..Default::default()
-        },
-        ..Default::default()
-    });
+        })
+        .insert(Player)
+        .insert(PlayerReadyToFire(true))
+        .insert(Speed::default());
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Speed, &mut Transform), With<Player>>,
+) {
+    if let Ok((speed, mut transform)) = query.single_mut() {
+        let dir = if keyboard_input.pressed(KeyCode::Left) {
+            -1.
+        } else if keyboard_input.pressed(KeyCode::Right) {
+            1.
+        } else {
+            0.
+        };
+
+        transform.translation.x += dir * speed.0 * TIME_STEP;
+    }
+}
+
+fn player_fire(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    materials: Res<Materials>,
+    mut query: Query<(&Transform, &mut PlayerReadyToFire), With<Player>>,
+) {
+    if let Ok((player_transform, mut fire)) = query.single_mut() {
+        if fire.0 && keyboard_input.pressed(KeyCode::Space) {
+            let x = player_transform.translation.x;
+            let y = player_transform.translation.y;
+
+            commands
+                .spawn_bundle(SpriteBundle {
+                    material: materials.laser.clone(),
+                    transform: Transform {
+                        translation: Vec3::new(x, y + 15., 0.),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .insert(Laser)
+                .insert(Speed::default());
+            fire.0 = false;
+        }
+
+        if keyboard_input.just_released(KeyCode::Space) {
+            fire.0 = true;
+        }
+    }
+}
+
+fn laser_movement(
+    mut commands: Commands,
+    window: Res<WinSize>,
+    mut query: Query<(Entity, &Speed, &mut Transform), With<Laser>>,
+) {
+    for (laser_entity, speed, mut laser_transform) in query.iter_mut() {
+        let translation = &mut laser_transform.translation;
+        translation.y += speed.0 * TIME_STEP;
+        if translation.y > window.h {
+            commands.entity(laser_entity).despawn();
+        }
+    }
 }
