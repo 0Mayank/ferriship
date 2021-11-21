30e905b45f1b30e9beb5900c64e6fb4ade4fd38d
Mayank Tomar <40265635+0Mayank@users.noreply.github.com>
2021-11-21 18:10:01 +0530

    refractored into different systems

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,17 @@ use bevy::prelude::*;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 
+// region: Resources
+pub struct Materials {
+    player_materials: Handle<ColorMaterial>,
+}
+
+pub struct WinSize {
+    h: f32,
+    w: f32,
+}
+// endregion: Resources
+
 fn main() {
     App::build()
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
@@ -15,6 +26,10 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
+        .add_startup_stage(
+            "game_setup_actors",
+            SystemStage::single(player_spawn.system()),
+        )
         .run();
 }
 
@@ -29,10 +44,22 @@ fn setup(
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
+    // create the main resources
+    commands.insert_resource(Materials {
+        player_materials: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+    });
+
+    commands.insert_resource(WinSize {
+        h: window.height(),
+        w: window.width(),
+    })
+}
+
+fn player_spawn(mut commands: Commands, materials: Res<Materials>, window: Res<WinSize>) {
     // spawn a sprite
-    let bottom = -window.height() / 2.;
+    let bottom = -window.h / 2.;
     commands.spawn_bundle(SpriteBundle {
-        material: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        material: materials.player_materials.clone(),
         transform: Transform {
             translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
             scale: Vec3::new(0.5, 0.5, 1.),
